a108b03ffe5d2b0e085efdf8848966cee1ace0c0
Kitty cat <kremlin9@mail.com>
2013-09-07 02:31:50 +0400

    add hours stat

---
+++ src/Stat/GeneralBundle/Entity/Hour.php
@@ -0,0 +1,251 @@
+<?php
+
+namespace Stat\GeneralBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Hour
+ */
+class Hour
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var integer
+     */
+    private $hit;
+
+    /**
+     * @var integer
+     */
+    private $uniq;
+
+    /**
+     * @var integer
+     */
+    private $newUsers;
+
+    /**
+     * @var integer
+     */
+    private $transactionsCount;
+
+    /**
+     * @var integer
+     */
+    private $money;
+
+    /**
+     * @var integer
+     */
+    private $payer;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set hit
+     *
+     * @param integer $hit
+     * @return Hour
+     */
+    public function setHit($hit)
+    {
+        $this->hit = $hit;
+    
+        return $this;
+    }
+
+    /**
+     * Get hit
+     *
+     * @return integer 
+     */
+    public function getHit()
+    {
+        return $this->hit;
+    }
+
+    /**
+     * Set uniq
+     *
+     * @param integer $uniq
+     * @return Hour
+     */
+    public function setUniq($uniq)
+    {
+        $this->uniq = $uniq;
+    
+        return $this;
+    }
+
+    /**
+     * Get uniq
+     *
+     * @return integer 
+     */
+    public function getUniq()
+    {
+        return $this->uniq;
+    }
+
+    /**
+     * Set newUsers
+     *
+     * @param integer $newUsers
+     * @return Hour
+     */
+    public function setNewUsers($newUsers)
+    {
+        $this->newUsers = $newUsers;
+    
+        return $this;
+    }
+
+    /**
+     * Get newUsers
+     *
+     * @return integer 
+     */
+    public function getNewUsers()
+    {
+        return $this->newUsers;
+    }
+
+    /**
+     * Set transactionsCount
+     *
+     * @param integer $transactionsCount
+     * @return Hour
+     */
+    public function setTransactionsCount($transactionsCount)
+    {
+        $this->transactionsCount = $transactionsCount;
+    
+        return $this;
+    }
+
+    /**
+     * Get transactionsCount
+     *
+     * @return integer 
+     */
+    public function getTransactionsCount()
+    {
+        return $this->transactionsCount;
+    }
+
+    /**
+     * Set money
+     *
+     * @param integer $money
+     * @return Hour
+     */
+    public function setMoney($money)
+    {
+        $this->money = $money;
+    
+        return $this;
+    }
+
+    /**
+     * Get money
+     *
+     * @return integer 
+     */
+    public function getMoney()
+    {
+        return $this->money;
+    }
+
+    /**
+     * Set payer
+     *
+     * @param integer $payer
+     * @return Hour
+     */
+    public function setPayer($payer)
+    {
+        $this->payer = $payer;
+    
+        return $this;
+    }
+
+    /**
+     * Get payer
+     *
+     * @return integer 
+     */
+    public function getPayer()
+    {
+        return $this->payer;
+    }
+    /**
+     * @var string
+     */
+    private $game;
+
+
+    /**
+     * Set game
+     *
+     * @param string $game
+     * @return Hour
+     */
+    public function setGame($game)
+    {
+        $this->game = $game;
+    
+        return $this;
+    }
+
+    /**
+     * Get game
+     *
+     * @return string 
+     */
+    public function getGame()
+    {
+        return $this->game;
+    }
+    /**
+     * @var \DateTime
+     */
+    private $created_at;
+
+
+    /**
+     * Set created_at
+     *
+     * @param \DateTime $createdAt
+     * @return Hour
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->created_at = $createdAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get created_at
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+}
