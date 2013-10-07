56e3026de2ef65cb287d2659dec678ba23aeeb72
Kitty cat <kremlin9@mail.com>
2013-10-08 01:16:46 +0400

    fix

--- src/Stat/GeneralBundle/Controller/DefaultController.php
+++ src/Stat/GeneralBundle/Controller/DefaultController.php
@@ -45,7 +45,7 @@ class DefaultController extends Controller {
             $in_out[$game]['in']  = $in;
             $in_out[$game]['out'] = $out;
             $in_out[$game]['diff'] = $in + $out;
-            $in_out[$game]['diff_p'] = $in ? ($in + $out) * 100 / $in : 0;
+            $in_out[$game]['diff_p'] = sprintf("%.2f", $in ? ($in + $out) * 100 / $in : 0);
 
             $banks[$game] = $bank;
         }
